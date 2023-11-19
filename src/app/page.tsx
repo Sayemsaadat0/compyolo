029c38bf0a5c9815d05655be4766d543a0478843
Sayemsaadat0 <sayemsaadat0@gmail.com>
2023-11-19 09:50:05 +0600

    formik

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import FunctionalNavbar from "@/components/core/navbar/FunctionalNavbar";
 import Navigationslider from "@/components/core/slider/Navigationslider";
 import SliderCard from "@/components/core/slider/SliderCard";
 import Slidercustom from "@/components/core/slider/Slidercustom";
@@ -5,8 +6,8 @@ import Slidercustom from "@/components/core/slider/Slidercustom";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between max-w-[1530px] mx-auto py-20  border-red-500 px-6 ">
+      {/* <FunctionalNavbar /> */}
       {/* <Slidercustom /> */}
-
       {/* <Navigationslider /> */}
     </main>
   );
